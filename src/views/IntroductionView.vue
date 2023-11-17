2d59e8ed9cc0fd4a975ac6bcefefbe9920bf4b4c
Asiimoviet <asiimoviet@gmail.com>
2023-11-17 03:01:45 -0800

    在Introduction页面增加了是否登录的提示

--- src/views/IntroductionView.vue
+++ src/views/IntroductionView.vue
@@ -3,10 +3,20 @@ import { useDark, useToggle } from '@vueuse/core'
 import { useUserStore } from "@/stores/user";
 import { useRouter } from "vue-router";
 import alice from '@/assets/alice.png'
+import { ref } from "vue";
 
 const isDark = useDark()
 const router = useRouter()
 const store = useUserStore()
+const loginDialogVisible = ref(false)
+
+function jumpToDashboard() {
+  if (!store.isLogin) {
+    loginDialogVisible.value = true
+  } else {
+    router.push('/assignments')
+  }
+}
 </script>
 
 <template>
@@ -19,9 +29,25 @@ const store = useUserStore()
     </el-col>
     <el-col><el-divider></el-divider></el-col>
     <el-col>
-      <el-button type="primary" size="large" @click="router.push('/assignments')">Go to Dashboard</el-button>
+      <el-button type="primary" size="large" @click="jumpToDashboard">Go to Dashboard</el-button>
     </el-col>
   </el-row>
+
+  <el-dialog
+    v-model="loginDialogVisible"
+    title="Login?"
+    width="30%"
+  >
+    <span>You are not signed in. If you proceed, you will visit as a guest and functionalities will be limited. Do you want to continue?</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="router.push('/assignments')">Proceed as Guest</el-button>
+        <el-button type="primary" @click="router.push('/login')">
+          Login
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
 </div>
 </template>
 
