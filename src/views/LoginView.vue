79b21f3f3de73f5809f241012686047e8d839874
Asiimoviet <asiimoviet@gmail.com>
2023-11-17 03:19:59 -0800

    完成了登录页面的UI

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -28,32 +28,50 @@ function login() {
 
 <template>
   <div class="login-form-container">
-    <el-form
-      ref="loginFormRef"
-      :model="loginForm"
-      :rules="rules"
-      label-width="120px"
-      status-icon
-    >
-      <el-form-item label="Username" prop="name">
-        <el-input v-model="loginForm.name" />
-      </el-form-item>
-      <el-form-item label="Password" prop="password">
-        <el-input v-model="loginForm.password" type="password" :show-password="true" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="login">Login</el-button>
-        <el-button>Register</el-button>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="text" @click="router.push('/assignments')">Continue as Guest</el-button>
-      </el-form-item>
-    </el-form>
+    <el-card class="login-card" shadow="always">
+      <div class="login-header-container"><el-text class="login-header">LeetScope</el-text></div>
+      <div class="flex-grow"></div>
+      <el-form
+          ref="loginFormRef"
+          :model="loginForm"
+          :rules="rules"
+          label-width="80px"
+          status-icon
+      >
+        <el-form-item label="Username" prop="name">
+          <el-input v-model="loginForm.name" />
+        </el-form-item>
+        <el-form-item label="Password" prop="password">
+          <el-input v-model="loginForm.password" type="password" :show-password="true" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="login">Login</el-button>
+          <el-button>Register</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="text" @click="router.push('/assignments')">Continue as Guest</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <style scoped>
 .login-form-container {
+  justify-content: center;
+}
+.login-header {
+  font-size: 28px;
+}
+.login-header-container {
+  margin-bottom: 20px;
+}
+.login-card {
   max-width: 400px;
+  padding: 20px;
+  margin: 10% auto auto;
+}
+.flex-grow {
+  flex-grow: 1;
 }
 </style>
