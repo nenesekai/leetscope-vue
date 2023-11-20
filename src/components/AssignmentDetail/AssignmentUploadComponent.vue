8bfccb3a694cd96c9ca13be9afbaeb7d259c2510
Asiimoviet <asiimoviet@gmail.com>
2023-11-19 16:55:39 -0800

    把AssignmentDetail的Upload模块分离出来

---
+++ src/components/AssignmentDetail/AssignmentUploadComponent.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { UploadFilled } from '@element-plus/icons-vue'
+import { useUserStore } from "@/stores/user";
+import { ref } from "vue";
+import { genFileId, UploadInstance, UploadProps, UploadRawFile } from "element-plus";
+
+const props = defineProps<{ assignment?: Assignment; user?: User }>()
+const store = useUserStore()
+const upload = ref<UploadInstance>()
+
+const handleExceed: UploadProps['onExceed'] = (files: File[]) => {
+  upload.value!.clearFiles()
+  const file = files[0] as UploadRawFile
+  file.uid = genFileId()
+  upload.value!.handleStart(file)
+}
+
+const submitHandler = () => {
+  upload.value!.submit()
+}
+
+const clearHandler = () => {
+  upload.value!.clearFiles()
+}
+</script>
+
+<template>
+  <div class="submit-area-container">
+    <el-upload
+      drag
+      ref="upload"
+      action="http://localhost:8080/submission/upload"
+      :headers="{ Authorization: 'Bearer ' + store.token }"
+      :data="{ assignmentId: assignment?.id }"
+      :multiple="false"
+      :limit="1"
+      :on-exceed="handleExceed"
+      :auto-upload="false"
+    >
+      <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+      <div class="el-upload__text">Submit your work here</div>
+    </el-upload>
+    <el-space>
+      <el-button @click="clearHandler">Clear</el-button>
+      <el-button type="primary" @click="submitHandler">Submit</el-button>
+    </el-space>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/AssignmentDetailView.vue
+++ src/views/AssignmentDetailView.vue
@@ -2,29 +2,18 @@
 import { useRoute, useRouter } from "vue-router";
 import api from "@/api";
 import type { AxiosError, AxiosResponse } from "axios";
-import { computed, ref } from "vue";
-import { UploadFilled } from '@element-plus/icons-vue'
+import { ref } from "vue";
 import { useUserStore } from "@/stores/user";
-import {
-  ElMessage, genFileId, UploadInstance,
-  UploadProps,
-  UploadRawFile,
-  UploadRequestHandler,
-  UploadRequestOptions,
-  UploadUserFile
-} from "element-plus";
-import AssignmentDetailsPageHeader from "@/components/AssignmentDetailsPageHeader.vue";
-import AssignmentIdInvalidError from "@/components/AssignmentIdInvalidError.vue";
+import { ElMessage } from "element-plus";
+import AssignmentDetailsPageHeader from "@/components/AssignmentDetail/AssignmentDetailsPageHeader.vue";
+import AssignmentIdInvalidError from "@/components/AssignmentDetail/AssignmentIdInvalidError.vue";
+import AssignmentUploadComponent from "@/components/AssignmentDetail/AssignmentUploadComponent.vue";
 
 const route = useRoute()
-const router = useRouter()
 const assignment = ref<Assignment>()
-const upload = ref<UploadInstance>()
-
-const invalid = ref(false)
 const user = ref<User>()
+const invalid = ref(false)
 const store = useUserStore()
-let files: UploadUserFile[] = []
 if (store.isLogin) {
   api.getCurrentUser().then((response: AxiosResponse) => {
     user.value = response.data.data
@@ -39,21 +28,6 @@ api.getAssignmentById(Number(route.params.id)).then((response: AxiosResponse) =>
     invalid.value = true
   }
 })
-
-const handleExceed: UploadProps['onExceed'] = (files: File[]) => {
-  upload.value!.clearFiles()
-  const file = files[0] as UploadRawFile
-  file.uid = genFileId()
-  upload.value!.handleStart(file)
-}
-
-const submitHandler = () => {
-  upload.value!.submit()
-}
-
-const clearHandler = () => {
-  upload.value!.clearFiles()
-}
 </script>
 
 <template>
@@ -61,29 +35,7 @@ const clearHandler = () => {
   <div class="assignment-detail-container" v-else>
     <AssignmentDetailsPageHeader :assignment="assignment" :user="user" />
 
-    <div class="submit-area-container" v-if="user != undefined && user.isStudent">
-      <el-upload
-        drag
-        ref="upload"
-        action="http://localhost:8080/submission/upload"
-        :headers="{ Authorization: 'Bearer ' + store.token }"
-        :data="{ assignmentId: assignment?.id }"
-        :file-list="files"
-        :multiple="false"
-        :limit="1"
-        :on-exceed="handleExceed"
-        :auto-upload="false"
-      >
-        <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-        <div class="el-upload__text">
-          Submit your work here
-        </div>
-      </el-upload>
-      <el-space>
-        <el-button @click="clearHandler">Clear</el-button>
-        <el-button type="primary" @click="submitHandler">Submit</el-button>
-      </el-space>
-    </div>
+    <AssignmentUploadComponent :assignment="assignment" :user="user" v-if="user != undefined && user.isStudent" />
   </div>
 </template>
 
