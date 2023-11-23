5a42357364a3c1f56fdb45ea22581d1f7fa1ca68
Asiimoviet <asiimoviet@gmail.com>
2023-11-22 16:25:33 -0800

    修改了No Pass不显示的bug

--- src/components/AssignmentDetail/SubmissionCardComponent.vue
+++ src/components/AssignmentDetail/SubmissionCardComponent.vue
@@ -10,14 +10,14 @@ const props = defineProps<{submission: Submission}>()
         <el-button class="button" text>Operation button</el-button>
       </div>
     </template>
-    <el-descriptions>
+    <el-descriptions :column="4">
       <el-descriptions-item label="File Name">{{submission.fileName}}</el-descriptions-item>
       <el-descriptions-item label="Create Time">{{new Date(submission.createTime).toLocaleString()}}</el-descriptions-item>
       <el-descriptions-item label="Grade Status">
         <el-tag v-if="submission.isGraded">Graded</el-tag>
         <el-tag v-else type="danger">Not Graded</el-tag>
       </el-descriptions-item>
-      <el-descriptions-item v-if="submission.isPass" label="Result">
+      <el-descriptions-item v-if="submission.isGraded" label="Result">
         <el-tag v-if="submission.isPass">Pass</el-tag>
         <el-tag v-else type="danger">No Pass</el-tag>
       </el-descriptions-item>
